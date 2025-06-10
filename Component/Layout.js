ad9d3919c975ba09b307ad2b2c08c09f537efb25
punitdiwan <punitdiwan@gmail.com>
2025-06-10 14:49:54 +0530

    build with openssl-legacy provider

--- Component/Layout.js
+++ Component/Layout.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Head from 'next/head'
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-XXXXXXXXXX");
+ReactGA.initialize("G-8PFHLEVQ3C");
 const Layout = ({ children, header_data }) => {
   return (
     <div>
